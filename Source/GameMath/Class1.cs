f44a015dfe89d834c15b37950aaae3373582f5d8
Nick Prühs <dev@npruehs.de>
2015-10-29 09:26:33 +0100

    CHANGED: Project - Set target framework to 3.5 to be used with Unity. Removed unused library references.

--- Source/GameMath/Class1.cs
+++ Source/GameMath/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GameMath
+﻿namespace GameMath
 {
     public class Class1
     {
