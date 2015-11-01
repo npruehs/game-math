1d4adc232a0a23c96d5956ab2da3b769e996349f
Nick Prühs <dev@npruehs.de>
2015-11-01 12:16:25 +0100

    ADDED: Vectors - Two-dimensional float vector.

---
+++ Source/GameMath/MathF.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MathF.cs" company="Slash Games">
+//   Copyright (c) Slash Games. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace GameMath
+{
+    using System;
+
+    public class MathF
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Checks whether the two passed numbers are equal, with respect
+        ///   to possible loss of precision caused by rounding values.
+        /// </summary>
+        /// <param name="f">
+        ///   First number to compare.
+        /// </param>
+        /// <param name="g">
+        ///   Second number to compare.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c>, if both numbers are equal, and <c>false</c> otherwise.
+        /// </returns>
+        public static bool Equals(float f, float g)
+        {
+            return Math.Abs(f - g) < float.Epsilon;
+        }
+
+        /// <summary>
+        ///   Returns the square root of the specified number.
+        /// </summary>
+        /// <param name="f">
+        ///   Number to get the square root of.
+        /// </param>
+        /// <returns>
+        ///   Square root of the specified number
+        /// </returns>
+        public static float Sqrt(float f)
+        {
+            return (float)Math.Sqrt(f);
+        }
+
+        #endregion
+    }
+}
--- Source/GameMath/Properties/AssemblyInfo.cs
+++ Source/GameMath/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -36,3 +37,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("0.1.0.*")]
+
+[assembly: CLSCompliant(true)]
