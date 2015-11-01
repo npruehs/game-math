85089453009532b4bed9d5fa74dc4c9755469ba8
Nick Prühs <dev@npruehs.de>
2015-11-01 12:23:41 +0100

    ADDED: Vectors - Two-dimensional int vector.

--- Source/GameMath/Geometry/Vector2I.cs
+++ Source/GameMath/Geometry/Vector2I.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Vector3F.cs" company="Slash Games">
+// <copyright file="Vector2I.cs" company="Slash Games">
 //   Copyright (c) Slash Games. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
@@ -9,22 +9,22 @@ namespace GameMath
     using System;
 
     /// <summary>
-    ///   Vector in three-dimensional space with floating point components. Note that vectors are immutable.
+    ///   Vector in two-dimensional space with integer components. Note that vectors are immutable.
     /// </summary>
     [CLSCompliant(true)]
-    public struct Vector3F : IEquatable<Vector3F>
+    public struct Vector2I : IEquatable<Vector2I>
     {
         #region Constants
 
         /// <summary>
         ///   Vector with all components set to one.
         /// </summary>
-        public static readonly Vector3F One = new Vector3F(1.0f, 1.0f, 1.0f);
+        public static readonly Vector2I One = new Vector2I(1, 1);
 
         /// <summary>
         ///   Null vector.
         /// </summary>
-        public static readonly Vector3F Zero = new Vector3F();
+        public static readonly Vector2I Zero = new Vector2I();
 
         #endregion
 
@@ -33,17 +33,12 @@ namespace GameMath
         /// <summary>
         ///   X-component of this vector.
         /// </summary>
-        private readonly float x;
+        private readonly int x;
 
         /// <summary>
         ///   Y-component of this vector.
         /// </summary>
-        private readonly float y;
-
-        /// <summary>
-        ///   Z-component of this vector.
-        /// </summary>
-        private readonly float z;
+        private readonly int y;
 
         #endregion
 
@@ -58,15 +53,11 @@ namespace GameMath
         /// <param name="y">
         ///   Y-component of the new vector.
         /// </param>
-        /// <param name="z">
-        ///   Z-component of the new vector.
-        /// </param>
-        public Vector3F(float x, float y, float z)
+        public Vector2I(int x, int y)
             : this()
         {
             this.x = x;
             this.y = y;
-            this.z = z;
         }
 
         #endregion
@@ -87,18 +78,18 @@ namespace GameMath
         /// <summary>
         ///   Gets the squared magnitude of this vector. Faster than <see cref="Length" />.
         /// </summary>
-        public float LengthSquared
+        public int LengthSquared
         {
             get
             {
-                return (this.x * this.x) + (this.y * this.y) + (this.z * this.z);
+                return (this.x * this.x) + (this.y * this.y);
             }
         }
 
         /// <summary>
         ///   Gets the x-component of this vector.
         /// </summary>
-        public float X
+        public int X
         {
             get
             {
@@ -109,22 +100,11 @@ namespace GameMath
         /// <summary>
         ///   Gets the y-component of this vector.
         /// </summary>
-        public float Y
+        public int Y
         {
             get
             {
                 return this.y;
-            }
-        }
-
-        /// <summary>
-        ///   Gets the z-component of this vector.
-        /// </summary>
-        public float Z
-        {
-            get
-            {
-                return this.z;
             }
         }
 
@@ -144,7 +124,7 @@ namespace GameMath
         /// <returns>
         ///   Sum of the passed vectors.
         /// </returns>
-        public static Vector3F Add(Vector3F v1, Vector3F v2)
+        public static Vector2I Add(Vector2I v1, Vector2I v2)
         {
             return v1 + v2;
         }
@@ -158,45 +138,9 @@ namespace GameMath
         /// <returns>
         ///   Sum of both vectors.
         /// </returns>
-        public Vector3F Add(Vector3F v)
+        public Vector2I Add(Vector2I v)
         {
             return Add(this, v);
-        }
-
-        /// <summary>
-        ///   Computes the cross product of the passed vectors. See
-        ///   http://en.wikipedia.org/wiki/Cross_product for details.
-        /// </summary>
-        /// <param name="v1">
-        ///   First vector to compute the cross product of.
-        /// </param>
-        /// <param name="v2">
-        ///   Second vector to compute the cross product of.
-        /// </param>
-        /// <returns>
-        ///   Cross product of the passed vectors.
-        /// </returns>
-        public static Vector3F Cross(Vector3F v1, Vector3F v2)
-        {
-            return new Vector3F(
-                (v1.y * v2.z) - (v1.z * v2.y),
-                (v1.z * v2.x) - (v1.x * v2.z),
-                (v1.x * v2.y) - (v1.y * v2.x));
-        }
-
-        /// <summary>
-        ///   Computes the cross product of the passed vector and this one. See
-        ///   http://en.wikipedia.org/wiki/Cross_product for details.
-        /// </summary>
-        /// <param name="v">
-        ///   Vector to compute the cross product of.
-        /// </param>
-        /// <returns>
-        ///   Cross product of the both vectors.
-        /// </returns>
-        public Vector3F Cross(Vector3F v)
-        {
-            return Cross(this, v);
         }
 
         /// <summary>
@@ -211,7 +155,7 @@ namespace GameMath
         /// <returns>
         ///   Euclidean distance between the two passed points.
         /// </returns>
-        public static float Distance(Vector3F v1, Vector3F v2)
+        public static float Distance(Vector2I v1, Vector2I v2)
         {
             return MathF.Sqrt(DistanceSquared(v1, v2));
         }
@@ -225,14 +169,14 @@ namespace GameMath
         /// <returns>
         ///   Euclidean distance between the two points.
         /// </returns>
-        public float Distance(Vector3F v)
+        public float Distance(Vector2I v)
         {
             return Distance(this, v);
         }
 
         /// <summary>
         ///   Computes the squared Euclidean distance between the points at <paramref name="v1" /> and <paramref name="v2" />.
-        ///   Faster than <see cref="Distance(Vector3F, Vector3F)" />.
+        ///   Faster than <see cref="Distance(Vector2I, Vector2I)" />.
         /// </summary>
         /// <param name="v1">
         ///   First point to compute the squared distance of.
@@ -243,17 +187,16 @@ namespace GameMath
         /// <returns>
         ///   Squared Euclidean distance between the two passed points.
         /// </returns>
-        public static float DistanceSquared(Vector3F v1, Vector3F v2)
+        public static int DistanceSquared(Vector2I v1, Vector2I v2)
         {
             var distX = v1.x - v2.x;
             var distY = v1.y - v2.y;
-            var distZ = v1.z - v2.z;
-            return (distX * distX) + (distY * distY) + (distZ * distZ);
+            return (distX * distX) + (distY * distY);
         }
 
         /// <summary>
-        ///   Computes the squared Euclidean distance between the points denoted by this vector and <paramref name="v" />.
-        ///   Faster than <see cref="Distance(Vector3F)" />.
+        ///   Computes the squared Euclidean distance between the points denoted by this vector and <paramref name="v" />. Faster
+        ///   than <see cref="Distance(Vector2I)" />.
         /// </summary>
         /// <param name="v">
         ///   Point to compute the squared distance to.
@@ -261,7 +204,7 @@ namespace GameMath
         /// <returns>
         ///   Squared Euclidean distance between the two points.
         /// </returns>
-        public float DistanceSquared(Vector3F v)
+        public int DistanceSquared(Vector2I v)
         {
             return DistanceSquared(this, v);
         }
@@ -272,29 +215,29 @@ namespace GameMath
         /// <param name="v">
         ///   Dividend.
         /// </param>
-        /// <param name="f">
+        /// <param name="i">
         ///   Divisor.
         /// </param>
         /// <returns>
         ///   Vector divided by the specified scalar.
         /// </returns>
-        public static Vector3F Divide(Vector3F v, float f)
+        public static Vector2I Divide(Vector2I v, int i)
         {
-            return v / f;
+            return v / i;
         }
 
         /// <summary>
         ///   Divides this vector by the specified scalar.
         /// </summary>
-        /// <param name="f">
+        /// <param name="i">
         ///   Divisor.
         /// </param>
         /// <returns>
         ///   Vector divided by the specified scalar.
         /// </returns>
-        public Vector3F Divide(float f)
+        public Vector2I Divide(int i)
         {
-            return Divide(this, f);
+            return Divide(this, i);
         }
 
         /// <summary>
@@ -309,9 +252,9 @@ namespace GameMath
         /// <returns>
         ///   Dot product of the two passed vectors.
         /// </returns>
-        public static float Dot(Vector3F v1, Vector3F v2)
+        public static int Dot(Vector2I v1, Vector2I v2)
         {
-            return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+            return (v1.x * v2.x) + (v1.y * v2.y);
         }
 
         /// <summary>
@@ -323,7 +266,7 @@ namespace GameMath
         /// <returns>
         ///   Dot product of the two vectors.
         /// </returns>
-        public float Dot(Vector3F v)
+        public int Dot(Vector2I v)
         {
             return Dot(this, v);
         }
@@ -344,7 +287,7 @@ namespace GameMath
                 return false;
             }
 
-            return obj is Vector3F && this.Equals((Vector3F)obj);
+            return obj is Vector2I && this.Equals((Vector2I)obj);
         }
 
         /// <summary>
@@ -356,9 +299,9 @@ namespace GameMath
         /// <returns>
         ///   <c>true</c>, if both vectors are equal, and <c>false</c> otherwise.
         /// </returns>
-        public bool Equals(Vector3F other)
+        public bool Equals(Vector2I other)
         {
-            return this.x.Equals(other.x) && this.y.Equals(other.y) && this.z.Equals(other.z);
+            return this.x.Equals(other.x) && this.y.Equals(other.y);
         }
 
         /// <summary>
@@ -373,72 +316,31 @@ namespace GameMath
             {
                 var hashCode = this.x.GetHashCode();
                 hashCode = (hashCode * 397) ^ this.y.GetHashCode();
-                hashCode = (hashCode * 397) ^ this.z.GetHashCode();
                 return hashCode;
             }
         }
 
         /// <summary>
-        ///   Linearly interpolates between the two passed vectors.
-        /// </summary>
-        /// <param name="v1">
-        ///   First vector to interpolate.
-        /// </param>
-        /// <param name="v2">
-        ///   Second vector to interpolate.
-        /// </param>
-        /// <param name="l">
-        ///   Interpolation parameter. 0 returns <paramref name="v1" />, 1 returns <paramref name="v2" />.
-        /// </param>
-        /// <returns>
-        ///   Linear interpolation between the two passed vectors.
-        /// </returns>
-        public static Vector3F Lerp(Vector3F v1, Vector3F v2, float l)
-        {
-            var lerpX = MathF.Lerp(v1.x, v2.x, l);
-            var lerpY = MathF.Lerp(v1.y, v2.y, l);
-            var lerpZ = MathF.Lerp(v1.z, v2.z, l);
-            return new Vector3F(lerpX, lerpY, lerpZ);
-        }
-
-        /// <summary>
-        ///   Linearly interpolates between the passed vector and this one.
-        /// </summary>
-        /// <param name="v">
-        ///   Vector to interpolate.
-        /// </param>
-        /// <param name="l">
-        ///   Interpolation parameter. 0 returns this vector, 1 returns <paramref name="v" />.
-        /// </param>
-        /// <returns>
-        ///   Linear interpolation between the two vectors.
-        /// </returns>
-        public Vector3F Lerp(Vector3F v, float l)
-        {
-            return Lerp(this, v, l);
-        }
-
-        /// <summary>
         ///   Multiplies the passed vector with the specified scalar.
         /// </summary>
         /// <param name="v">
         ///   Vector to multiply.
         /// </param>
-        /// <param name="f">
+        /// <param name="i">
         ///   Scalar to multiply the vector with.
         /// </param>
         /// <returns>
         ///   Product of the vector and the scalar.
         /// </returns>
-        public static Vector3F Multiply(Vector3F v, float f)
+        public static Vector2I Multiply(Vector2I v, int i)
         {
-            return f * v;
+            return i * v;
         }
 
         /// <summary>
         ///   Multiplies the passed vector with the specified scalar.
         /// </summary>
-        /// <param name="f">
+        /// <param name="i">
         ///   Scalar to multiply the vector with.
         /// </param>
         /// <param name="v">
@@ -447,58 +349,23 @@ namespace GameMath
         /// <returns>
         ///   Product of the vector and the scalar.
         /// </returns>
-        public static Vector3F Multiply(float f, Vector3F v)
+        public static Vector2I Multiply(int i, Vector2I v)
         {
-            return f * v;
+            return i * v;
         }
 
         /// <summary>
         ///   Multiplies this vector with the specified scalar.
         /// </summary>
-        /// <param name="f">
+        /// <param name="i">
         ///   Scalar to multiply this vector with.
         /// </param>
         /// <returns>
         ///   Product of this vector and the scalar.
         /// </returns>
-        public Vector3F Multiply(float f)
+        public Vector2I Multiply(int i)
         {
-            return Multiply(f, this);
-        }
-
-        /// <summary>
-        ///   Normalizes the passed vector, returning a unit vector with the same orientation.
-        ///   If the passed vector is the zero vector, the zero vector is returned instead.
-        /// </summary>
-        /// <param name="v">
-        ///   Vector to normalize.
-        /// </param>
-        /// <returns>
-        ///   Normalized passed vector.
-        /// </returns>
-        public static Vector3F Normalize(Vector3F v)
-        {
-            var lengthSquared = v.LengthSquared;
-            if (MathF.Equals(lengthSquared, 0f) || MathF.Equals(lengthSquared, 1f))
-            {
-                return v;
-            }
-
-            var lengthInverse = 1.0f / MathF.Sqrt(lengthSquared);
-
-            return new Vector3F(v.x * lengthInverse, v.y * lengthInverse, v.z * lengthInverse);
-        }
-
-        /// <summary>
-        ///   Normalizes this vector, returning a unit vector with the same orientation.
-        ///   If this passed vector is the zero vector, the zero vector is returned instead.
-        /// </summary>
-        /// <returns>
-        ///   Normalized vector.
-        /// </returns>
-        public Vector3F Normalize()
-        {
-            return Normalize(this);
+            return Multiply(i, this);
         }
 
         /// <summary>
@@ -513,9 +380,9 @@ namespace GameMath
         /// <returns>
         ///   Sum of the passed vectors.
         /// </returns>
-        public static Vector3F operator +(Vector3F v1, Vector3F v2)
+        public static Vector2I operator +(Vector2I v1, Vector2I v2)
         {
-            return new Vector3F(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+            return new Vector2I(v1.x + v2.x, v1.y + v2.y);
         }
 
         /// <summary>
@@ -524,15 +391,15 @@ namespace GameMath
         /// <param name="v">
         ///   Dividend.
         /// </param>
-        /// <param name="f">
+        /// <param name="i">
         ///   Divisor.
         /// </param>
         /// <returns>
         ///   Vector divided by the specified scalar.
         /// </returns>
-        public static Vector3F operator /(Vector3F v, float f)
+        public static Vector2I operator /(Vector2I v, int i)
         {
-            return new Vector3F(v.x / f, v.y / f, v.z / f);
+            return new Vector2I(v.x / i, v.y / i);
         }
 
         /// <summary>
@@ -547,7 +414,7 @@ namespace GameMath
         /// <returns>
         ///   <c>true</c>, if both vectors are equal, and <c>false</c> otherwise.
         /// </returns>
-        public static bool operator ==(Vector3F v1, Vector3F v2)
+        public static bool operator ==(Vector2I v1, Vector2I v2)
         {
             return v1.Equals(v2);
         }
@@ -564,7 +431,7 @@ namespace GameMath
         /// <returns>
         ///   <c>true</c>, if the vectors are not equal, and <c>false</c> otherwise.
         /// </returns>
-        public static bool operator !=(Vector3F v1, Vector3F v2)
+        public static bool operator !=(Vector2I v1, Vector2I v2)
         {
             return !(v1 == v2);
         }
@@ -575,21 +442,21 @@ namespace GameMath
         /// <param name="v">
         ///   Vector to multiply.
         /// </param>
-        /// <param name="f">
+        /// <param name="i">
         ///   Scalar to multiply the vector with.
         /// </param>
         /// <returns>
         ///   Product of the vector and the scalar.
         /// </returns>
-        public static Vector3F operator *(Vector3F v, float f)
+        public static Vector2I operator *(Vector2I v, int i)
         {
-            return f * v;
+            return i * v;
         }
 
         /// <summary>
         ///   Multiplies the passed vector with the specified scalar.
         /// </summary>
-        /// <param name="f">
+        /// <param name="i">
         ///   Scalar to multiply the vector with.
         /// </param>
         /// <param name="v">
@@ -598,9 +465,9 @@ namespace GameMath
         /// <returns>
         ///   Product of the vector and the scalar.
         /// </returns>
-        public static Vector3F operator *(float f, Vector3F v)
+        public static Vector2I operator *(int i, Vector2I v)
         {
-            return new Vector3F(v.x * f, v.y * f, v.z * f);
+            return new Vector2I(v.x * i, v.y * i);
         }
 
         /// <summary>
@@ -615,9 +482,9 @@ namespace GameMath
         /// <returns>
         ///   Difference of both vectors.
         /// </returns>
-        public static Vector3F operator -(Vector3F v1, Vector3F v2)
+        public static Vector2I operator -(Vector2I v1, Vector2I v2)
         {
-            return new Vector3F(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+            return new Vector2I(v1.x - v2.x, v1.y - v2.y);
         }
 
         /// <summary>
@@ -632,7 +499,7 @@ namespace GameMath
         /// <returns>
         ///   Difference of both vectors.
         /// </returns>
-        public static Vector3F Subtract(Vector3F v1, Vector3F v2)
+        public static Vector2I Subtract(Vector2I v1, Vector2I v2)
         {
             return v1 - v2;
         }
@@ -646,7 +513,7 @@ namespace GameMath
         /// <returns>
         ///   Difference of both vectors.
         /// </returns>
-        public Vector3F Subtract(Vector3F v)
+        public Vector2I Subtract(Vector2I v)
         {
             return Subtract(this, v);
         }
@@ -659,7 +526,7 @@ namespace GameMath
         /// </returns>
         public override string ToString()
         {
-            return string.Format("({0}, {1}, {2})", this.x, this.y, this.z);
+            return string.Format("({0}, {1})", this.x, this.y);
         }
 
         #endregion
