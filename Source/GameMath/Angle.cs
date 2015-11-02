12251a1f3e18766d4e08f7ee0759eec58725eee6
Nick Prühs <dev@npruehs.de>
2015-11-02 22:25:35 +0100

    ADDED: Operations - Trigonometric functions.

--- Source/GameMath/Angle.cs
+++ Source/GameMath/Angle.cs
@@ -11,6 +11,70 @@
         #region Public Methods and Operators
 
         /// <summary>
+        ///   Inverse of the cos function.
+        ///   Returns the angle whose cosine is the specified number.
+        /// </summary>
+        /// <param name="x">Number to get the acos of.</param>
+        /// <returns>Angle whose cosine is the specified number.</returns>
+        public static float Acos(float x)
+        {
+            return (float)Math.Acos(x);
+        }
+
+        /// <summary>
+        ///   Inverse of the sin function.
+        ///   Returns the angle whose sine is the specified number.
+        /// </summary>
+        /// <param name="x">Number to get the asin of.</param>
+        /// <returns>Angle whose sine is the specified number.</returns>
+        public static float Asin(float x)
+        {
+            return (float)Math.Asin(x);
+        }
+
+        /// <summary>
+        ///   Inverse of the tan function.
+        ///   Returns the angle whose tangent is the specified number.
+        /// </summary>
+        /// <param name="x">Number to get the atan of.</param>
+        /// <returns>Angle whose tangent is the specified number.</returns>
+        public static float Atan(float x)
+        {
+            return (float)Math.Atan(x);
+        }
+
+        /// <summary>
+        ///   Returns the angle whose tangent is the quotient of two specified numbers.
+        /// </summary>
+        /// <param name="y">Dividend.</param>
+        /// <param name="x">Divisor.</param>
+        /// <returns>Angle whose tangent is the quotient of two specified numbers.</returns>
+        public static float Atan2(float y, float x)
+        {
+            return (float)Math.Atan2(y, x);
+        }
+
+        /// <summary>
+        ///   Returns the cosine of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the cosine of.</param>
+        /// <returns>Cosine of the specified angle.</returns>
+        public static float Cos(float x)
+        {
+            return (float)Math.Cos(x);
+        }
+
+        /// <summary>
+        ///   Returns the hyperbolic cosine of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the hyperbolic cosine of.</param>
+        /// <returns>Hyperbolic cosine of the specified angle.</returns>
+        public static float Cosh(float x)
+        {
+            return (float)Math.Cosh(x);
+        }
+
+        /// <summary>
         ///   Converts the specified angle in degrees to radians.
         /// </summary>
         /// <param name="degrees">
@@ -36,6 +100,46 @@
         public static float RadiansToDegrees(float degrees)
         {
             return degrees * (180.0f / Constants.Pi);
+        }
+
+        /// <summary>
+        ///   Returns the sine of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the sine of.</param>
+        /// <returns>Sine of the specified angle.</returns>
+        public static float Sin(float x)
+        {
+            return (float)Math.Sin(x);
+        }
+
+        /// <summary>
+        ///   Returns the hyperbolic sine of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the hyperbolic sine of.</param>
+        /// <returns>Hyperbolic sine of the specified angle.</returns>
+        public static float Sinh(float x)
+        {
+            return (float)Math.Sinh(x);
+        }
+
+        /// <summary>
+        ///   Returns the tangent of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the tangent of.</param>
+        /// <returns>Tangent of the specified angle.</returns>
+        public static float Tan(float x)
+        {
+            return (float)Math.Tan(x);
+        }
+
+        /// <summary>
+        ///   Returns the hyperbolic tangent of the specified angle.
+        /// </summary>
+        /// <param name="x">Angle to get the hyperbolic tangent of.</param>
+        /// <returns>Hyperbolic tangent of the specified angle.</returns>
+        public static float Tanh(float x)
+        {
+            return (float)Math.Tanh(x);
         }
 
         #endregion
