027dc1a9ff43e9a3490f0eacda6b5982ca1d2f97
Nick Prühs <dev@npruehs.de>
2015-11-10 23:14:31 +0100

    ADDED: Core - Cardinal directions.

---
+++ Source/GameMath.Tests/Core/DirectionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace GameMath.Tests.Core
+{
+    using NUnit.Framework;
+
+    public class DirectionTests
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void TestIsDiagonal()
+        {
+            Assert.IsTrue(Direction.SouthEast.IsDiagonal());
+        }
+
+        [Test]
+        public void TestIsHorizontal()
+        {
+            Assert.IsTrue(Direction.West.IsHorizontal());
+        }
+
+        [Test]
+        public void TestIsNotDiagonal()
+        {
+            Assert.IsFalse(Direction.South.IsDiagonal());
+        }
+
+        [Test]
+        public void TestIsNotHorizontal()
+        {
+            Assert.IsFalse(Direction.South.IsHorizontal());
+        }
+
+        [Test]
+        public void TestIsNotVertical()
+        {
+            Assert.IsFalse(Direction.West.IsVertical());
+        }
+
+        [Test]
+        public void TestIsVertical()
+        {
+            Assert.IsTrue(Direction.North.IsVertical());
+        }
+
+        [Test]
+        public void TestOppositeDirection()
+        {
+            Assert.AreEqual(Direction.West, Direction.East.Opposite());
+            Assert.AreEqual(Direction.NorthEast, Direction.SouthWest.Opposite());
+        }
+
+        #endregion
+    }
+}
