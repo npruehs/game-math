31b81ab97425c3ae2d234ce6e02671adcdfd05bc
Nick Prühs <dev@npruehs.de>
2015-11-10 22:23:51 +0100

    CLOSED #6 2D Intersection - Line-Circle

---
+++ Source/GameMath.Tests/Intersections/LineIntersectionTests.cs
@@ -0,0 +1,32 @@
+﻿namespace GameMath.Tests.Intersections
+{
+    using NUnit.Framework;
+
+    public class LineIntersectionTests
+    {
+        #region Public Methods and Operators
+
+        [Test]
+        public void TestLineIntersectsCircle()
+        {
+            // ARRANGE.
+            var line = new LineSegment2F(new Vector2F(-2.0f, 0.0f), new Vector2F(2.0f, 0.0f));
+            var circle = CircleF.UnitCircle;
+
+            Vector2F? first;
+            Vector2F? second;
+
+            // ACT.
+            var intersects = line.Intersects(circle, out first, out second);
+
+            // ASSERT.
+            Assert.IsTrue(intersects);
+            Assert.IsNotNull(first);
+            Assert.IsNotNull(second);
+            Assert.AreEqual(new Vector2F(1.0f, 0.0f), first);
+            Assert.AreEqual(new Vector2F(-1.0f, 0.0f), second);
+        }
+
+        #endregion
+    }
+}
---
+++ Source/GameMath/Intersections/CircleIntersection.cs
@@ -0,0 +1,59 @@
+﻿namespace GameMath
+{
+    using System;
+
+    /// <summary>
+    ///   Intersection tests for circles.
+    /// </summary>
+    [CLSCompliant(true)]
+    public static class CircleIntersection
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///   Checks whether the specified circle intersects the passed line.
+        /// </summary>
+        /// <param name="circle">
+        ///   Circle to check.
+        /// </param>
+        /// <param name="line">
+        ///   Line to check.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c>, if circle and line intersect each other, and <c>false</c> otherwise.
+        /// </returns>
+        public static bool Intersects(this CircleF circle, LineSegment2F line)
+        {
+            return line.Intersects(circle);
+        }
+
+        /// <summary>
+        ///   Checks whether the specified circle intersects the passed line.
+        /// </summary>
+        /// <param name="circle">
+        ///   Circle to check.
+        /// </param>
+        /// <param name="line">
+        ///   Line to check.
+        /// </param>
+        /// <param name="first">
+        ///   First intersection point, if found.
+        /// </param>
+        /// <param name="second">
+        ///   Second intersection point, if found.
+        /// </param>
+        /// <returns>
+        ///   <c>true</c>, if circle and line intersect each other, and <c>false</c> otherwise.
+        /// </returns>
+        public static bool Intersects(
+            this CircleF circle,
+            LineSegment2F line,
+            out Vector2F? first,
+            out Vector2F? second)
+        {
+            return line.Intersects(circle, out first, out second);
+        }
+
+        #endregion
+    }
+}
