690a2f305ece6e7af55c486cbec626757e49f163
Nick Prühs <dev@npruehs.de>
2016-05-16 14:45:47 +0200

    CHANGED: Project - Updated assembly copyright.

--- Source/GameMath/Properties/AssemblyInfo.cs
+++ Source/GameMath/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open Source Developers")]
 [assembly: AssemblyProduct("GameMath")]
-[assembly: AssemblyCopyright("Copyright 2015")]
+[assembly: AssemblyCopyright("Copyright 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
